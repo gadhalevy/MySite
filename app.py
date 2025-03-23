7ceff1da9b29f8e4d015e32e90f016c9466cbf4b
gadhalevy <gad3dh@gmail.com>
2025-03-23 15:46:31 +0200

    Update app.py
    Add secrets, and colored txt_input for email

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
+from streamlit_extras.stylable_container import  stylable_container
 
 
 def to_hebrew():
@@ -50,9 +51,10 @@ def send_email(receiver, files=None):
     :param files:
     :return:
     '''
-    with open('passtxt.txt') as f:
-        password = f.read()
-    # password=st.secrets.sisma
+    # with open('passtxt.txt') as f:
+    #     password = f.read()
+
+    password=st.secrets.password
     sender = 'khanuka1912@gmail.com'
     body = "צורפת לרשימת התפוצה של Puzzle Mind"
     # Create a multipart message and set headers
@@ -110,15 +112,26 @@ def fill_container(pic, wrt1, wrt2, wrt3, mrkdn, img, txt1, txt2, txt3, txt4, co
 
 
 to_hebrew()
-# set_bg("https://iris-bs.co.il/wp-content/uploads/2021/01/100111.jpg")
+set_bg("https://iris-bs.co.il/wp-content/uploads/2021/01/100111.jpg")
 col1, col2, col3 = st.columns([1.32, 2, 1.32])
 col2.header(':flag-il: :rainbow[Puzzle Mind] :flag-il:', divider=True)
 col1, col2 = st.columns([1, 1])
 col1.subheader(' פאזל מיינד מציעה משחקי חשיבה מאתגרים המפתחים יכולות קוגנטיביות ומחזקים את הזיכרון. המשחקים מציעים מגוון רחב של אתגרים המותאמים לכל הרמות והגילים.')
-col2.image("mankhe.png")
-st.write('ליצירת קשר אנא צרף אימייל')
+col2.image("assets/images/mankhe.png")
+col1.write('ליצירת קשר אנא צרף אימייל')
 col3, col4 = st.columns([1, 4])
-receiver = col4.text_input(' ', label_visibility='collapsed')
+# receiver = col4.text_input(' ', label_visibility='collapsed')
+# receiver=col4.markdown(f'<input type="text" style="background-color:coral"/input>',unsafe_allow_html=True)
+with col4:
+    with stylable_container(
+    key="email",
+    css_styles='''
+    input{
+        type:text;
+        background-color:coral;
+        }
+    ''',):
+        receiver=st.text_input(' ', label_visibility='collapsed')
 send_button = col3.button('שלח!', use_container_width=True, type='primary')
 if send_button:
     msg = send_email(receiver)
@@ -132,21 +145,21 @@ txt11 = 'הנאה צרופה'
 txt12 = 'חוויה מעצימה ומגבשת'
 txt13 = 'משחקי חברה חכמים ומענינים'
 c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1 = setup_container(col1)
-img = 'hanaa.png'
+img = 'assets/images/hanaa.png'
 txt2 = ''
 txt3 = ''
 txt4 = 'חוויה מהנה ובלתי נשכחת שתגרום לכם לצחוק להנות ולהתחבר יחד.'
 spaces = len(txt13) - len(txt11)
 fill_container(c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1, img, txt11, txt2, txt3, txt4, 'yellow', spaces=spaces)
 c2_pic, c2_wrt1, c2_wrt2, c2_wrt3, c2_mrkdn1 = setup_container(col2)
-img = 'havaia.png'
+img = 'assets/images/havaia.png'
 txt2 = ''
 txt3 = ''
 txt4 = 'פיתוח חשיבה יצירתית עבודת צוות ופתרון בעיות מורכבות בדרך מהנה ומאתגרת.'
 spaces = len(txt13) - len(txt12)
 fill_container(c2_pic, c2_wrt1, c2_wrt2, c2_wrt3, c2_mrkdn1, img, txt12, txt2, txt3, txt4, 'cyan', spaces=spaces)
 c3_pic, c3_wrt1, c3_wrt2, c3_wrt3, c3_mrkdn1 = setup_container(col3)
-img = 'haham.png'
+img = 'assets/images/haham.png'
 txt2 = ''
 # txt3=''
 txt4 = 'מגוון רחב של משחקים חברתיים המשלבים איסטרטגיה יצירתיות ואינטרקציה חברתית.'
@@ -156,24 +169,26 @@ col2.write('מה יש לנו להציע?')
 col1, col2, col3 = st.columns([2, 3, 2])
 options = ['תשבץ הגיון', 'זיהוי דמות', 'זיהוי מקום']
 res = col2.segmented_control('המשחקים שלנו', options=options)
+col1,col2=st.columns([10,0.1])
+c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1 = setup_container(col1)
 if res == 'תשבץ הגיון':
-    img = 'higayon.png'
+    img = 'assets/images/higayon.png'
     txt1 = 'תשבץ הגיון'
     txt2 = ''
     txt3 = ''
     txt4 = 'תשבץ הגיון מבוסס הקשר לארוע מסוים או לקבוצה מסוימת מוקרן על מסך גדול המנצח הינו זה הפותר את מירב ההגדרות.'
-    fill_container(c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1, img, txt1, txt2, txt3, txt4, 'yellow', 200)
+    fill_container(c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1, img, txt1, txt2, txt3, txt4, 'yellow', 600)
 if res == 'זיהוי דמות':
-    img = 'mi.png'
+    img = 'assets/images/mi.png'
     txt1 = 'מי הדמות'
     txt2 = ''
     txt3 = ''
     txt4 = 'פלטפורמה של משחקים שבה המשתמש כותב חידה על דמות בטלפון החידה מוקרנת על מסך גדול ושאר המשתתפים פותרים כל חידה בתורה המנצח הינו זה שזיהה את מירב הדמויות .'
-    fill_container(c2_pic, c2_wrt1, c2_wrt2, c2_wrt3, c2_mrkdn1, img, txt1, txt2, txt3, txt4, 'cyan', 200)
+    fill_container(c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1, img, txt1, txt2, txt3, txt4, 'cyan', 600)
 if res == 'זיהוי מקום':
-    img = 'place.png'
+    img = 'assets/images/place.png'
     txt1 = 'זיהוי מקום'
     txt2 = ''
     txt3 = ''
     txt4 = 'המערכת בוחרת יישוב מסוים בארץ כמטרה, כל משתתף בוחר ישוב מסוים דרך הטלפון ומקבל חיווי על המרחק והכיוון של הבחירה שלו מהמטרה המנצח הינו הקרוב ביותר למטרה.'
-    fill_container(c3_pic, c3_wrt1, c3_wrt2, c3_wrt3, c3_mrkdn1, img, txt1, txt2, txt3, txt4, 'greenyellow', 200)
+    fill_container(c1_pic, c1_wrt1, c1_wrt2, c1_wrt3, c1_mrkdn1, img, txt1, txt2, txt3, txt4, 'greenyellow', 600)
